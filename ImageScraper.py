866dd5b4933b68ca53a3cd115cacdd197e775556
Bryan Vartanian <48264008+NukingKing@users.noreply.github.com>
2020-12-23 05:55:54 -0800

    Update ImageScraper.py
    Cleaned up code

--- ImageScraper.py
+++ ImageScraper.py
@@ -9,7 +9,7 @@ reddit = praw.Reddit(client_id = '6oQuWT8j5mrj0g',
                      user_agent = 'GeminiScraper')
 
 image_urls = []
-for post in reddit.subreddit("AnimeWallpaper").new(limit=10):
+for post in reddit.subreddit("AnimeWallpaper").new(limit=1):
     image_urls.append(post.url.encode('utf-8'))
     
 print(image_urls)
@@ -18,23 +18,3 @@ file = open("sample_image.png", "wb")
 file.write(response.content)
 file.close()
 
-#print(submission.title)
-
-# subreddit = reddit.subreddit('animewallpaper') 
-# posts = subreddit.hot(limit=10)
-
-# image_urls = []
-# image_titles = []
-# image_scores = []
-# image_timestamps = []
-# image_ids = []
-
-
-# for post in posts:
-#   image_urls.append(post.url.encode('utf-8'))
-#   image_titles.append(post.title.encode('utf-8'))
-#   image_scores.append(post.score)
-#   image_timestamps.append(datetime.datetime.fromtimestamp(post.created))
-#   image_ids.append(post.id)
-
-# print(image_titles)
