97da6bc4506eac1e748a34162633de43a1b6447e
Bryan Vartanian <48264008+NukingKing@users.noreply.github.com>
2020-12-31 16:29:03 -0800

    GS-11
    Changed to 10 Images and Saves in Image folder

--- ImageScraper.py
+++ ImageScraper.py
@@ -1,19 +1,29 @@
 
+#"pip install praw" in console to install praw
 import praw
 import urllib
 import requests
+import os.path
 
-reddit = praw.Reddit(client_id = '6oQuWT8j5mrj0g', 
-                     client_secret = 'lwCsLOu6b9Doayj96CHyuCWJo0fVtg', 
+#Login infor to use Praw in reddit. Created and registered an app in reddit
+reddit = praw.Reddit(client_id = '6oQuWT8j5mrj0g',
+                     client_secret = 'lwCsLOu6b9Doayj96CHyuCWJo0fVtg',
                      user_agent = 'GeminiScraper')
 
 image_urls = []
-for post in reddit.subreddit("AnimeWallpaper").new(limit=1):
-    image_urls.append(post.url.encode('utf-8'))
-    
+#takes posts from subreddit and adds to list
+for post in reddit.subreddit("AnimeWallpaper").new(limit=10):
+    image_urls.append(post.url)
+
+
 print(image_urls)
-response = requests.get(image_urls[0])
-file = open("sample_image.png", "wb")
-file.write(response.content)
-file.close()
+folder = 'Image/'
+#saves images to folder
+for x in range(len(image_urls)):
+    imageName = image_urls[x]
+    response = requests.get(image_urls[x])
+    fullpath = os.path.join('Images/',str(imageName[-17:]))
+    file = open(fullpath, "wb")
+    file.write(response.content)
+    file.close()
 
