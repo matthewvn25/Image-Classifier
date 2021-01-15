c9bb0e2bf968995850a219806cc0ec5bf5ada976
matthewvn25 <33705607+matthewvn25@users.noreply.github.com>
2021-01-14 16:25:33 -0800

    Centroids and Image Comparisons Live
    1. Centroids can now be created.
    2. Images of different resolutions can now be compared.
    3. Cleaned and commented code.

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -4,84 +4,130 @@ from PIL import ImageChops
 from numpy import asarray
 
 import sys
+import os, numpy
 
 #from scipy.misc import imread
 from scipy.linalg import norm
 from scipy import sum, average
-# =============================================================================
-# im2 = Image.open("Images\Image2.png")
-# im3 = Image.open("Images\Image3.png")
-# 
-# diff = ImageChops.difference(im2, im3)
-# 
-# print('new line')
-# print(diff)
-# 
-# diff.save("Output/out.jpg", "JPEG")
-# =============================================================================
 
 def main():
-    print("hello")
-    #testDict()
-    #testPixelChange()
-    source = Image.open("Images\Image6.png")
-    #mostSimilar(source)
-    similarityPercentage(source)
-
-def testPixelChange():
-    im2 = Image.open("Images\Image2.png")
+    if os.path.exists("TestSet"): #more for the download
+        #createCentroidsHelper()
+        testImageNames = os.listdir("TestSet")
+        for name in testImageNames:
+            print(name)
+            similarityPercentage(Image.open("TestSet/" + name))
+            print()
+    else:
+        print("TestSet file does not exist. Ending program.")
     
-    pixels = im2.load() # crate the pixel map
-    z = 0
-    for i in range (im2.size[0]):
-        for j in range(im2.size[1]):
-            z = z + 1
-            #pixels[i,j] = (i, j, 100) #set color
-            #print(pixels[i,j][0])
-            
-    im2.save("Output/im2.jpg", "JPEG")
-
-def mostSimilar(source):
-
-    source_pixels = source.load()
-    dict = {}
-    for k in range (7): #for each image in the folder
-        imgName = "Image" + str(k) + ".png"
-        img = Image.open("Images\\" + imgName)
-        img_pixels = img.load()
-        totalDiff = 0
-        for i in range (source.size[0]): # for each row in the image
-            for j in range(source.size[1]): # for each col in the image
-                totalDiff = totalDiff + abs(source_pixels[i,j][0] - img_pixels[i,j][0]) + \
-                    abs(source_pixels[i,j][1] - img_pixels[i,j][1]) + \
-                    abs(source_pixels[i,j][2] - img_pixels[i,j][2])
-        dict[imgName] = totalDiff
-        #dict[imgName] = totalDiff / (source.size[0] * source.size[1])
-    for key in dict:
-        print(key + ": " + str(dict[key]))
-    
-#research eigen values
 def similarityPercentage(source):
-    similarityPercentage = {}
-    for k in range (7): #for each image in the folder
-        imageName = "Image" + str(k) + ".png"
-        img = Image.open("Images\\" + imageName)
-        absDiff = ImageChops.difference(source, img)
-        absDiff_pixel = absDiff.load()
-        zeroCount = 0
+    """Prints the similarity percentages between source image and centroids
+    The source image and centroids are transformed into the same resolution.
+    The absolute difference between the source image and each centroid is taken.
+    The absolute difference, an Image object, is iterated through pixel by pixel to calculate the similarity between each pixel.
+    
+    :param source : source image to compare centroids to
+    """
+    
+    dict = {}
+    names = os.listdir("Centroids")
+    centroids = []
+    for name in names:
+        centroids.append(Image.open("Centroids/" + name))
         
-        for i in range (absDiff.size[0]): # for each row in the image
-            for j in range(absDiff.size[1]): # for each col in the image
-                if(absDiff_pixel[i,j] == (0,0,0)):
-                    zeroCount = zeroCount + 1
-        similarityPercentage[imageName] = zeroCount * 100 / (absDiff.size[0] * absDiff.size[1])
+    for k in range (len(centroids)):
+        smallestRes = findSmallestResolution(source, centroids[k])
+        source = source.resize(smallestRes, Image.ANTIALIAS)
+        centroids[k] = centroids[k].resize(smallestRes, Image.ANTIALIAS)
         
-    for key in similarityPercentage:
-        print(key + ": " + str(similarityPercentage[key]) + "%")
+        source_pixels = source.load()
+        centroid_pixels = centroids[k].load()
+        
+        #obtain absolute difference of 2 images
+        absDiff = ImageChops.difference(source, centroids[k])
+        absDiff_pixels = absDiff.load()
+        
+        similarPixelCount = 0
+        for i in range (smallestRes[0]): 
+            for j in range(smallestRes[1]):
+                #The pixel absolute distance of 100 was chosen arbitrarily. Subject to change.
+                if(absDiff_pixels[i,j][0] + absDiff_pixels[i,j][1] + absDiff_pixels[i,j][2] <= 100):
+                     similarPixelCount =  similarPixelCount + 1
+                     
+        dict[names[k]] = round(similarPixelCount / (smallestRes[0] * smallestRes[1]) * 100, 3) 
+        
+    for key in dict:
+        print("\t" + key + ": " + str(dict[key]) + "%")
+        
+def findSmallestResolution(imageList):
+    """Returns the smallest resolution from a List of Image Objects.
+    Each resolution is calculated by multiplying the Image Object's length and width
+    
+    :param imageList : a List of Image Objects
+    :return smallestRes : the smallest resolution as a tuple
+    """
+    
+    numOfImages = len(imageList)
+    if(numOfImages <= 0):
+        return (0,0)
+    else:
+        smallestRes = imageList[0].size
+        for i in range (1, numOfImages):
+            if(smallestRes[0] * smallestRes[1] > imageList[i].size[0] * imageList[i].size[1]):
+                smallestRes = imageList[i].size
+        return smallestRes
+    
+def findSmallestResolution(img1, img2):
+    """Returns the smallest resolution of 2 Image Objects
+    Each resolution is calculated by multiplying the Image Object's length and width
+    
+    :param img1 : first image
+    :param img2 : second image
+    :return smallestRes : the smallest resolution as a tuple
+    """
+    
+    smallestRes = img1.size
+    if(img1.size[0] * img1.size[1] > img2.size[0] * img2.size[1]):
+        smallestRes = img2.size
+    return smallestRes
+    
+def createCentroidsHelper():
+    """Does preparation work before creating centroids
+    """
+    
+    categories = os.listdir("LearningSets")
+    for category in categories:
+        imageNames = os.listdir("LearningSets/" + category)
+        imageList = []
+        for image in imageNames:
+            imageList.append(Image.open("LearningSets/" + category + "/" + image))
+            
+        centroid = createCentroid(imageList, findSmallestResolution(imageList))
+        #centroid.show()
+        centroid.save("Centroids/centroid_" + category + ".png", "PNG")  
+        
+def createCentroid(imageList, smallestRes):
+    """Returns the centroid (average image) from a List of Image Objects.
+    
+    :param imageList : a List of Image Objects
+    :param smallestRes : the smallest resolution in imageList
+    :return out : the average of all images in imageList as the size of smallestRes
+    """
+    
+    N = len(imageList)
+    #create float array to hold pixel values
+    arr = numpy.zeros((smallestRes[1], smallestRes[0], 3), numpy.float)
+    
+    for image in imageList:
+        resized = image.resize(smallestRes, Image.ANTIALIAS)
+        #convert resized image into float array
+        imarr = numpy.array(resized, dtype = numpy.float)
+        arr = arr + imarr/N
                 
-def testDict():
-    dict = {"key" : 2, "sick" : 66}
-    dict ["new key"] = 55
-    print(dict)
-            #pixels[i,j] = (i, j, 100) #set color
+    #convert and round float array to int
+    arr = numpy.array(numpy.round(arr), dtype = numpy.uint8)
+    out = Image.fromarray(arr, mode = "RGB")
+    return out
+
 main()
